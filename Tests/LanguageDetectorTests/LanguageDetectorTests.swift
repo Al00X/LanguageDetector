883d3d29d4a95b3d8801d43bb32ed7e5f62015e1
Hadi Sharghi <hsharghi@gmail.com>
2023-12-20 17:06:36 +0330

    use ResourceManager singleton code refactoring

--- Tests/LanguageDetectorTests/LanguageDetectorTests.swift
+++ Tests/LanguageDetectorTests/LanguageDetectorTests.swift
@@ -3,43 +3,43 @@ import XCTest
 
 final class LanguageDetectorTests: XCTestCase {
     func testGetResourceData() throws {
-        let farsi = try ResourceManager.getResource(name: "fa")
+        let farsi = try ResourceManager.default.getResource(name: "fa")
         XCTAssertGreaterThan(farsi.frequency.count, 0)
         XCTAssertGreaterThan(farsi.nWords.count, 0)
         XCTAssertEqual(farsi.name, "fa")
     }
 
     func testCreate() throws {
-        let detector = Detector(langs: ["fa", "en", "ar"]);
-        detector.addLang(langs: ["fr", "ja"]);
+        let detector = Detector(langs: ["fa", "en", "ar"])
+        detector.addLang(["fr", "ja"])
 
-        XCTAssertEqual(detector.loadedSubsets.count, 5);
+        XCTAssertEqual(detector.loadedSubsets.count, 5)
     }
 
     func testEvaluate() throws {
         let detector = Detector(langs: ["fa", "en", "ar"])
-        detector.addLang(langs: ["ja", "it"])
-        let scoresEn = detector.evaluate(text: "Hi there fellow adventurer. how are you today?, please go and sit over there.");
+        detector.addLang(["ja", "it"])
+        let scoresEn = detector.evaluate(text: "Hi there fellow adventurer. how are you today?, please go and sit over there.")
         let scoresFa = detector.evaluate(text: "سلام ای پهلوان ایرانی، شنیده ام که کیفت کوک است!")
         
-        XCTAssertEqual(scoresEn?[0].0, "en")
-        XCTAssertEqual(scoresFa?[0].0, "fa")
+        XCTAssertEqual(scoresEn?.first?.0, "en")
+        XCTAssertEqual(scoresFa?.first?.0, "fa")
     }
 
     func testStaticDetect() throws {
         let scores = Detector.detect(text: "سلام ای پهلوان ایرانی، شنیده ام که کیفت کوک است!",langs: ["fa", "en", "ar"])
         
-        XCTAssertEqual(scores?[0].0, "fa")
+        XCTAssertEqual(scores?.first?.0, "fa")
     }
 
     func testEvaluateExperimental() throws {
         let detector = Detector(langs: ["fa", "en", "ar"])
-        detector.addLang(langs: ["ja", "it"])
-        let scoresEn = detector.evaluate(text: "Hi there fellow adventurer.");
+        detector.addLang(["ja", "it"])
+        let scoresEn = detector.evaluate(text: "Hi there fellow adventurer.")
         let scoresFa = detector.evaluate(text: "سلام ای پهلوان ایرانی!")
         
         // These two will fail
-        XCTAssertEqual(scoresEn?[0].0, "en") // it
-        XCTAssertEqual(scoresFa?[0].0, "fa") // ar
+        XCTAssertEqual(scoresEn?.first?.0, "en") // it
+        XCTAssertEqual(scoresFa?.first?.0, "fa") // ar
     }
 }
