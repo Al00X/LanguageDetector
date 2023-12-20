b10ebe9c1426bbcd8abd579ed61e06a311f65697
Ali X <al00x@outlook.com>
2023-12-20 10:34:05 +0330

    Updated detector & finilized

--- Sources/LanguageDetector/LanguageDetector.swift
+++ Sources/LanguageDetector/LanguageDetector.swift
@@ -4,7 +4,8 @@
 import Foundation
 
 class ResourceManager {
-    
+    static var loadedResources: [String: Subset] = [:]
+
     struct Subset: Codable {
         var frequency: [String: Int]
         var name: String
@@ -17,14 +18,14 @@ class ResourceManager {
         }
     }
     
-    
     static func getResource(name: String) throws -> Subset {
+        if loadedResources[name] != nil {
+            return loadedResources[name]!;
+        }
         let configURL = Bundle.module.url(forResource: name, withExtension: "")
         let data = try Data(contentsOf: configURL!)
         let json = try JSONDecoder().decode(Subset.self, from: data)
-        print("count1: \(json.frequency.count)")
-        print("count2: \(json.nWords.count)")
-        print("name: \(json.name)")
+        loadedResources[name] = json;
         return json
     }
     
@@ -33,6 +34,11 @@ class ResourceManager {
 class Detector {
     var loadedLangs: [String] = [];
     var loadedSubsets: [ResourceManager.Subset] = [];
+
+    static func detect(text: String, langs: [String]) -> [(String, Double)]? {
+        let instance = Detector(langs: langs);
+        return instance.evaluate(text: text);
+    }
 
     init(langs: [String]?) {
         self.addLang(langs: langs ?? []);
@@ -48,5 +54,40 @@ class Detector {
                 print("Error loading language resource subset: \(lang)", error);
             }            
         }
+    }
+
+    func evaluate(text: String) -> [(String, Double)]? {
+        if text.count == 0 {
+            return nil;
+        }
+        var scores: [(String, Double)] = [];
+        for lang in loadedSubsets {
+            let score = self.calculate(subset: lang, chunks: self.chunk(text: text))
+            scores.append((lang.name, score));
+        }
+        return scores.sorted(by: { $0.1 > $1.1 });
+    }
+
+    private func calculate(subset: ResourceManager.Subset, chunks: [String]) -> Double {
+        var freqSum = 0.0;
+        for chunk in chunks {
+            freqSum += Double(subset.frequency[chunk] ?? 0);
+        }
+        let nWordsSum = subset.nWords.reduce(0, { a, b in a + b})
+        return freqSum / Double(nWordsSum);
+    }
+
+    private func chunk(text: String) -> [String] {
+        var chunks: [String] = [];
+        
+        for i in 0..<3 {
+            for j in 0..<text.count {
+                if (text.count > j + i) {
+                    chunks.append(text[(j)...(j+i)])
+                }
+            }
+        }
+
+        return chunks;
     }
 }
--- Tests/LanguageDetectorTests/LanguageDetectorTests.swift
+++ Tests/LanguageDetectorTests/LanguageDetectorTests.swift
@@ -9,10 +9,26 @@ final class LanguageDetectorTests: XCTestCase {
         XCTAssertEqual(farsi.name, "fa")
     }
 
-    func testCreateLanguageDetector() throws {
+    func testCreate() throws {
         let detector = Detector(langs: ["fa", "en", "ar"]);
         detector.addLang(langs: ["fr", "ja"]);
 
         XCTAssertEqual(detector.loadedSubsets.count, 5);
+    }
+
+    func testEvaluate() throws {
+        let detector = Detector(langs: ["fa", "en", "ar"])
+        detector.addLang(langs: ["ja", "it"])
+        let scoresEn = detector.evaluate(text: "Hi there fellow adventurer. how are you today?, please go and sit over there.");
+        let scoresFa = detector.evaluate(text: "سلام ای پهلوان ایرانی، شنیده ام که کیفت کوک است!")
+        
+        XCTAssertEqual(scoresEn?[0].0, "en")
+        XCTAssertEqual(scoresFa?[0].0, "fa")
+    }
+
+    func testStaticDetect() throws {
+        let scores = Detector.detect(text: "سلام ای پهلوان ایرانی، شنیده ام که کیفت کوک است!",langs: ["fa", "en", "ar"])
+        
+        XCTAssertEqual(scores?[0].0, "fa")
     }
 }
