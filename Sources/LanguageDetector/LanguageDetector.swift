fddc421fa58eaaa46bc607c64b6472b8aacc2edf
Hadi Sharghi <hsharghi@gmail.com>
2023-12-20 17:06:03 +0330

    add singleton instance to ResourceManager some code refactoring

--- Sources/LanguageDetector/LanguageDetector.swift
+++ Sources/LanguageDetector/LanguageDetector.swift
@@ -3,8 +3,14 @@
 
 import Foundation
 
+
+enum DetectorError: String, Error {
+    case resourceNotFound
+}
+
 class ResourceManager {
-    static var loadedResources: [String: Subset] = [:]
+    static let `default` = ResourceManager()
+    var loadedResources = [String: Subset]()
 
     struct Subset: Codable {
         var frequency: [String: Int]
@@ -18,68 +24,68 @@ class ResourceManager {
         }
     }
     
-    static func getResource(name: String) throws -> Subset {
-        if loadedResources[name] != nil {
-            return loadedResources[name]!;
+    func getResource(name: String) throws -> Subset {
+        guard loadedResources[name] == nil else {
+            return loadedResources[name]!
         }
-        let configURL = Bundle.module.url(forResource: name, withExtension: "")
-        let data = try Data(contentsOf: configURL!)
-        let json = try JSONDecoder().decode(Subset.self, from: data)
-        loadedResources[name] = json;
-        return json
+        guard let configURL = Bundle.module.url(forResource: name, withExtension: "") else {
+            throw DetectorError.resourceNotFound
+        }
+        let data = try Data(contentsOf: configURL)
+        let subset = try JSONDecoder().decode(Subset.self, from: data)
+        loadedResources[name] = subset
+        return subset
     }
     
 }
 
 class Detector {
-    var loadedLangs: [String] = [];
-    var loadedSubsets: [ResourceManager.Subset] = [];
+    var loadedLangs = [String]()
+    var loadedSubsets = [ResourceManager.Subset]()
 
     static func detect(text: String, langs: [String]) -> [(String, Double)]? {
-        let instance = Detector(langs: langs);
-        return instance.evaluate(text: text);
+        Detector(langs: langs).evaluate(text: text)
     }
 
-    init(langs: [String]?) {
-        self.addLang(langs: langs ?? []);
+    init(langs: [String] = []) {
+        self.addLang(langs)
     }
 
-    func addLang(langs: [String]) {
+    func addLang(_ langs: [String]) {
         for lang in langs {
             do {
-                let resource = try ResourceManager.getResource(name: lang);
-                self.loadedLangs.append(lang);
-                self.loadedSubsets.append(resource);
-            } catch let error {
-                print("Error loading language resource subset: \(lang)", error);
+                if !self.loadedLangs.contains(lang) {
+                    let resource = try ResourceManager.default.getResource(name: lang)
+                    self.loadedLangs.append(lang)
+                    self.loadedSubsets.append(resource)
+                }
+            } catch {
+                print("Error loading language resource subset: \(lang)", error)
             }            
         }
     }
 
     func evaluate(text: String) -> [(String, Double)]? {
-        if text.count == 0 {
-            return nil;
+        guard text.count > 0 else {
+            return nil
         }
-        let trimmedText = text.replacing(" ", with: "")
-        var scores: [(String, Double)] = [];
-        for lang in loadedSubsets {
-            let score = self.calculate(subset: lang, chunks: self.chunk(text: trimmedText))
-            scores.append((lang.name, score));
+        let trimmedText = text.trimmingCharacters(in: .whitespaces)
+        var scores = [(String, Double)]()
+        for subset in loadedSubsets {
+            let score = self.calculate(subset: subset, chunks: chunk(text: trimmedText))
+            scores.append((subset.name, score))
         }
-        return scores.sorted(by: { $0.1 > $1.1 });
+        return scores.sorted(by: { $0.1 > $1.1 })
     }
 
     private func calculate(subset: ResourceManager.Subset, chunks: [String]) -> Double {
-        var freqSum = 0.0;
-        for chunk in chunks {
-            freqSum += Double(subset.frequency[chunk] ?? 0);
-        }
-        let nWordsSum = subset.nWords.reduce(0, { a, b in a + b})
-        return freqSum / Double(nWordsSum);
+        let freqSum = chunks.reduce(0) { $0 + (subset.frequency[$1] ?? 0) }
+        let nWordsSum = subset.nWords.reduce(0) { $0 + $1 }
+        return Double(freqSum) / Double(nWordsSum)
     }
 
     private func chunk(text: String) -> [String] {
-        var chunks: [String] = [];
+        var chunks = [String]()
         
         for i in 0..<3 {
             for j in 0..<text.count {
@@ -89,6 +95,6 @@ class Detector {
             }
         }
 
-        return chunks;
+        return chunks
     }
 }
