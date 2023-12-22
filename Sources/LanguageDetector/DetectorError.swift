4f0eaaef325a15cb25b1b942041732e3c327f33f
Ali X <al00x@outlook.com>
2023-12-22 18:28:19 +0330

    Cleanup

--- Sources/LanguageDetector/DetectorError.swift
+++ Sources/LanguageDetector/DetectorError.swift
@@ -1,10 +1,3 @@
-//
-//  File.swift
-//  
-//
-//  Created by Hadi Sharghi on 12/20/23.
-//
-
 import Foundation
 
 enum DetectorError: String, Error {
--- Tests/LanguageDetectorTests/LanguageDetectorTests.swift
+++ Tests/LanguageDetectorTests/LanguageDetectorTests.swift
@@ -5,7 +5,6 @@ final class LanguageDetectorTests: XCTestCase {
     func testGetResourceData() throws {
         let farsi = try ResourceManager.default.getResource(name: "fa")
         XCTAssertGreaterThan(farsi.frequency.count, 0)
-        // XCTAssertGreaterThan(farsi.nWords.count, 0)
         XCTAssertEqual(farsi.name, "fa")
     }
 
