8f425def6a59c4956aa44fc807c3f6912f24122e
Ali X <al00x@outlook.com>
2023-12-22 19:11:36 +0330

    Added failing tests!

--- Tests/LanguageDetectorTests/LanguageDetectorTests.swift
+++ Tests/LanguageDetectorTests/LanguageDetectorTests.swift
@@ -38,4 +38,32 @@ final class LanguageDetectorTests: XCTestCase {
         
         XCTAssertEqual(scores?.first?.0, "fa")
     }
+
+    func testEvaluationConsistency() throws {
+        let detector = Detector(langs: ["fa", "en", "ar"])
+
+        let a1 = detector.evaluate(text: "سلام داداش چه خبر")
+        let a2 = detector.evaluate(text: "سلام داداش چه خبر")
+
+        XCTAssertEqual(a1?.first?.1, a2?.first?.1)
+    }
+
+    func testEvaluateVariations() throws {
+        let detector = Detector(langs: ["fa", "en", "ar"])
+
+        let a1 = detector.evaluate(text: "سلام داداش چه خبر")
+        let a2 = detector.evaluate(text: "سلام داداش، چه خبر؟")
+        let a3 = detector.evaluate(text: " سلام داداش، چه خبر")
+        let a4 = detector.evaluate(text: "سلام داداش چه خبر؟")
+        let a5 = detector.evaluate(text: "سلام داداش چه خبر؟ <3   ")
+        let a6 = detector.evaluate(text: "سلام داداش چه خبر؟ 09123456789")
+        // The results above are different, and the a1's score is greater than the other...
+        // This should never happen! the original transformer is changed to prevent this error
+
+        // XCTAssertEqual(a1?.first?.1, a2?.first?.1)
+        // XCTAssertEqual(a1?.first?.1, a3?.first?.1)
+        // XCTAssertEqual(a1?.first?.1, a4?.first?.1)
+        // XCTAssertEqual(a1?.first?.1, a5?.first?.1)
+        // XCTAssertEqual(a1?.first?.1, a6?.first?.1)
+    }
 }
