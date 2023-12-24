fef4f9eb867c9f917e6ab8ac8a2879513505212d
Hadi Sharghi <hsharghi@gmail.com>
2023-12-24 17:00:27 +0330

    remove try catch block to allow init method throws remove semi colons cleanup closures

--- Sources/LanguageDetector/LanguageDetector.swift
+++ Sources/LanguageDetector/LanguageDetector.swift
@@ -1,33 +1,29 @@
 import Foundation
-import Collections;
+import Collections
 
 class Detector {
     var loadedLangs = [String]()
     var loadedSubsets = [ResourceManager.Subset]()
 
-    let MIN_WORD_LENGTH = 1;
-    let MAX_WORD_LENGTH = 3;
-    let MAX_NGRAMS = 310;
+    let MIN_WORD_LENGTH = 1
+    let MAX_WORD_LENGTH = 3
+    let MAX_NGRAMS = 310
 
-    static func detect(text: String, langs: [String]) -> [(String, Double)]? {
-        Detector(langs: langs).evaluate(text: text)
+    public static func detect(text: String, languages: [String]) throws -> [(String, Double)]? {
+        try Detector(langs: languages).evaluate(text: text)
     }
 
-    init(langs: [String] = []) {
-        self.addLang(langs)
+    public init(langs: [String] = []) throws {
+        try self.addLang(langs)
     }
 
-    func addLang(_ langs: [String]) {
+    public func addLang(_ langs: [String]) throws {
         for lang in langs {
-            do {
-                if !self.loadedLangs.contains(lang) {
-                    let resource = try ResourceManager.default.getResource(name: lang)
-                    self.loadedLangs.append(lang)
-                    self.loadedSubsets.append(resource)
-                }
-            } catch {
-                print("Error loading language resource subset: \(lang)", error)
-            }            
+            if !self.loadedLangs.contains(lang) {
+                let resource = try ResourceManager.default.getResource(name: lang)
+                self.loadedLangs.append(lang)
+                self.loadedSubsets.append(resource)
+            }
         }
     }
 
@@ -36,48 +32,48 @@ class Detector {
             return nil
         }
         var scores = [(String, Double)]()
-        let tokens = tokenize(text);
+        let tokens = tokenize(text)
         for subset in loadedSubsets {
             let score = self.calculate(subset: subset, samples: sample(tokens: tokens))
             scores.append((subset.name, score))
         }
-        return scores.sorted(by: { $0.1 > $1.1 })
+        return scores.sorted { $0.1 > $1.1 }
     }
 
     private func calculate(subset: ResourceManager.Subset, samples: [String]) -> Double {
         // index is accumulated at the start of the loop, so we initiate it as -1
-        var index = -1;
-        var sum = 0;
+        var index = -1
+        var sum = 0
         for chunk in samples {
-            index+=1;
+            index+=1
             guard let freq = subset.frequency[chunk] else {
                 sum += MAX_NGRAMS
-                continue;
+                continue
             }
             sum += abs(index - freq)
         }
-        return 1.0 - (Double(sum) / Double(MAX_NGRAMS * index));
+        return 1.0 - (Double(sum) / Double(MAX_NGRAMS * index))
     }
 
     private func sample(tokens: [String]) -> [String] {
         guard tokens.count != 0 else {
-            return [];
+            return []
         }
 
-        var chunks = OrderedDictionary<Int, OrderedDictionary<String, Int>>();
-        var ngrams = OrderedDictionary<Int, OrderedDictionary<String, Double>>(); 
+        var chunks = OrderedDictionary<Int, OrderedDictionary<String, Int>>()
+        var ngrams = OrderedDictionary<Int, OrderedDictionary<String, Double>>() 
         for word in tokens {
             for i in MIN_WORD_LENGTH...MAX_WORD_LENGTH {
                 if chunks[i] == nil {
-                    chunks[i] = [:];
+                    chunks[i] = [:]
                 }
                 for j in 0..<word.count {
                     guard i + j - 1 < word.count else {
-                        break;
+                        break
                     }
-                    let part = word[(j)..<(j+i)];
-                    let currentValue = chunks[i]![part] ?? 0;
-                    chunks[i]![part] = currentValue+1;
+                    let part = word[(j)..<(j+i)]
+                    let currentValue = chunks[i]![part] ?? 0
+                    chunks[i]![part] = currentValue+1
                 }
             }
         }
@@ -86,30 +82,31 @@ class Detector {
             let sum = chunk.value.reduce(0) { $0 + $1.value }
             
             if (ngrams[chunk.key] == nil) {
-                ngrams[chunk.key] = [:];
+                ngrams[chunk.key] = [:]
             }
             for item in chunk.value {
-                ngrams[chunk.key]![item.key] = Double(item.value) / Double(sum);
+                ngrams[chunk.key]![item.key] = Double(item.value) / Double(sum)
             }
         }
 
         let merged: OrderedDictionary<String, Double> = ngrams.reduce(into: OrderedDictionary()) { pre, cur in
             for item in cur.value {
-                if item.key == "_" { continue };
-                pre[item.key] = item.value;
+                if item.key == "_" { continue }
+                pre[item.key] = item.value
             }   
         }
-        let result = merged.sorted(by: {a,b in a.value > b.value && a.key > b.key})
-            .map() { $0.key }
+        let result = merged.sorted { $0.value > $1.value && $0.key > $1.key }
+            .map { $0.key }
 
-        return result.count > MAX_NGRAMS ? Array(result[0...MAX_NGRAMS]) : result;
+        return result.count > MAX_NGRAMS ? Array(result[0...MAX_NGRAMS]) : result
     }
 
     private func tokenize(_ text: String) -> [String] {
         var transformedText = text.lowercased()
-        let unicodeRegex = try! Regex("[^\\p{L}\\s]");
-        transformedText.replace(unicodeRegex, with: "");
-        transformedText = transformedText.trimmingCharacters(in: .whitespacesAndNewlines);
+        let unicodeRegex = try! Regex("[^\\p{L}\\s]")
+        transformedText = transformedText
+            .replacing(unicodeRegex, with: "")
+            .trimmingCharacters(in: .whitespacesAndNewlines)
         
         // We should split the string with the following regex
         // But Negative-Look-Back is not supported on Swift :(
--- Tests/LanguageDetectorTests/LanguageDetectorTests.swift
+++ Tests/LanguageDetectorTests/LanguageDetectorTests.swift
@@ -7,24 +7,23 @@ final class LanguageDetectorTests: XCTestCase {
         XCTAssertGreaterThan(farsi.frequency.count, 0)
         XCTAssertEqual(farsi.name, "fa")
     }
-
+    
     func testCreate() throws {
-        let detector = Detector(langs: ["fa", "en", "ar"])
-        detector.addLang(["fr", "ja"])
-
+        let detector = try Detector(langs: ["fa", "en", "ar"])
+        try detector.addLang(["fr", "ja"])
+        
         XCTAssertEqual(detector.loadedSubsets.count, 5)
     }
-
+    
     func testEvaluate() throws {
-        let detector = Detector(langs: ["fa", "en", "ar"])
-        detector.addLang(["ja", "it"])
+        let detector = try Detector(langs: ["fa", "en", "ar", "ja", "it"])
         let a = detector.evaluate(text: "Hi there fellow adventurer. how are you today?, please go and sit over there.")
         let b = detector.evaluate(text: "سلام ای پهلوان ایرانی، شنیده ام که کیفت کوک است!")
         let c = detector.evaluate(text: "Hi there fellow adventurer.")
         let d = detector.evaluate(text: "سلام ای پهلوان ایرانی!")
         let e = detector.evaluate(text: "سلام داداش");
         let f = detector.evaluate(text: "Yo ma boi");
-
+        
         XCTAssertEqual(a?.first?.0, "en")
         XCTAssertEqual(b?.first?.0, "fa")
         XCTAssertEqual(c?.first?.0, "en")
@@ -32,26 +31,26 @@ final class LanguageDetectorTests: XCTestCase {
         XCTAssertEqual(e?.first?.0, "fa")
         XCTAssertEqual(f?.first?.0, "en")
     }
-
+    
     func testStaticDetect() throws {
-        let scores = Detector.detect(text: "سلام ای پهلوان ایرانی، شنیده ام که کیفت کوک است!",langs: ["fa", "en", "ar"])
+        let scores = try Detector.detect(text: "سلام ای پهلوان ایرانی، شنیده ام که کیفت کوک است!",languages: ["fa", "en", "ar"])
         
         XCTAssertEqual(scores?.first?.0, "fa")
     }
-
+    
     func testEvaluationConsistency() throws {
-        let detector = Detector(langs: ["fa"])
-
+        let detector = try Detector(langs: ["fa"])
+        
         let a1 = detector.evaluate(text: "سلام داداش چه خبر")
         let a2 = detector.evaluate(text: "سلام داداش چه خبر")
         // Used OrderedDictionary to fix the inconsistency problem
-
+        
         XCTAssertEqual(a1?.first?.1, a2?.first?.1)
     }
-
+    
     func testEvaluateVariations() throws {
-        let detector = Detector(langs: ["fa", "en", "ar"])
-
+        let detector = try Detector(langs: ["fa", "en", "ar"])
+        
         let a1 = detector.evaluate(text: "سلام داداش چه خبر")
         let a2 = detector.evaluate(text: "سلام داداش، چه خبر؟")
         let a3 = detector.evaluate(text: " سلام داداش، چه خبر")
@@ -60,7 +59,7 @@ final class LanguageDetectorTests: XCTestCase {
         let a6 = detector.evaluate(text: "سلام داداش چه خبر؟ 09123456789")
         // The results above are different, and the a1's score is greater than the other...
         // This should never happen! the original transformer is changed to prevent this error
-
+        
         XCTAssertEqual(a1?.first?.1, a2?.first?.1)
         XCTAssertEqual(a1?.first?.1, a3?.first?.1)
         XCTAssertEqual(a1?.first?.1, a4?.first?.1)
